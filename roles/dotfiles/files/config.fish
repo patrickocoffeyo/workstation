f98ab54b20aafc371a2164affa2c007bea166c1a
Patrick Coffey <patrickcoffey48@gmail.com>
2020-04-30 15:44:41 -0500

    update config.fish

--- roles/dotfiles/files/config.fish
+++ roles/dotfiles/files/config.fish
@@ -3,7 +3,11 @@ setenv EDITOR vim
 set -xg GOROOT $HOME/Projects/go
 set -xg PATH $GOROOT/bin $PATH
 set -xg PATH ~/.local/bin $PATH
-alias terminus=/home/pcoffey/terminus/vendor/bin/terminus
+alias coveralls="brave-browser ./__coverage__/lcov-report/index.html"           
+alias twatch="yarn unit --watch --no-coverage"                                                                                                       
+alias pbcopy="xclip -selection clipboard"                                       
+alias pbpaste="xclip -selection clipboard -o"                                   
+alias l=ls
 
 set fish_color_normal c5c8c6
 set fish_color_command b294bb
